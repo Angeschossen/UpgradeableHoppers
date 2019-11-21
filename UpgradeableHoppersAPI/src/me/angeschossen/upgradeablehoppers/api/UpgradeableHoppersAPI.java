b785b5db8a06305b275160a37e39c632bf8a7d11
Angeschossen <angeschossenofficial@gmail.com>
2019-11-21 10:55:30 +0100

    Changes

---
+++ UpgradeableHoppersAPI/src/me/angeschossen/upgradeablehoppers/api/UpgradeableHoppersAPI.java
@@ -0,0 +1,16 @@
+package me.angeschossen.upgradeablehoppers.api;
+
+import me.angeschossen.upgradeablehoppers.api.objects.Hopper;
+import org.bukkit.Location;
+
+public class UpgradeableHoppersAPI {
+
+
+    public static Hopper getHopper(Location location) {
+        return null;
+    }
+
+    public static boolean isHopper(Location location) {
+        return false;
+    }
+}
